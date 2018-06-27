92447d9aeb22a028cfa12c047f3b2fbab1fc3e50
Ben Cronin <benc@Bens-MacBook-Pro.local>
2018-06-27 12:36:22 -0700

    Set up and get VideoList, VideoListEntry, and VideoPlayer working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,13 @@ var Search = () => (
   <div className="search-bar form-inline">
     <input className="form-control" type="text" />
     <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
+      <span className="glyphicon glyphicon-search" />
     </button>
-  </div> 
+  </div>
 );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.Search = Search;
+
+// ReactDOM.render(<Search />, document.getElementById('search'));
