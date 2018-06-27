54972d3aa98f6890f243c351736e809e417f550a
samMahani <sam.amirmahani@gmail.com>
2018-06-27 16:26:05 -0700

    Get VideoPlayer to update on clicking VideoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,45 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div id="search">
-          <Search />
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData,
+      videoIndex: 0
+    };
+  }
+
+  clickVideo(index) {
+    console.log(index);
+    this.setState({ videoIndex: index });
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div id="search">
+              <Search />
+            </div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div id="player">
+              <VideoPlayer video={this.state.videos[this.state.videoIndex]} />
+            </div>
+          </div>
+          <div className="col-md-5">
+            <div id="list">
+              <VideoList videos={this.state.videos} clickCallback={this.clickVideo.bind(this)} />
+            </div>
+          </div>
         </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div id="player">
-          <VideoPlayer video={exampleVideoData[0]} />
-        </div>
-      </div>
-      <div className="col-md-5">
-        <div id="list">
-          <VideoList videos={exampleVideoData} />
-        </div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }  
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,7 @@
 var VideoList = props => (
   <div className="video-list">
     {props.videos.map((video, index) => (
-      <VideoListEntry key={index} video={video} />
+      <VideoListEntry key={index} index={index} video={video} clickCallback={props.clickCallback} />
     ))}
   </div>
 );
@@ -15,7 +15,3 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
-
-{
-  /* ReactDOM.render(<VideoList videos={exampleVideoData} />, document.getElementById('list')); */
-}
